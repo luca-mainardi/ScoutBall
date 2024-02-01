18999dc789690ec83bce701999ac7c31dcbe7931
zisisbilo <zisismp22@gmail.com>
2024-02-01 19:20:23 +0100

    update

--- jbi100_app/views/parallel_coordinates_chart.py
+++ jbi100_app/views/parallel_coordinates_chart.py
@@ -23,56 +23,7 @@ def build_parallel_coordinates_chart():
                         children=[
                             dcc.Graph(
                                 id="parallel-coord-chart",
-                                figure=go.Figure(
-                                    data=go.Parcoords(
-                                        line_color="blue",
-                                        dimensions=list(
-                                            [
-                                                dict(
-                                                    range=[1, 5],
-                                                    constraintrange=[
-                                                        1,
-                                                        2,
-                                                    ],  # change this range by dragging the pink line
-                                                    label="A",
-                                                    values=[1, 4],
-                                                ),
-                                                dict(
-                                                    range=[1.5, 5],
-                                                    tickvals=[
-                                                        1.5,
-                                                        3,
-                                                        4.5,
-                                                    ],
-                                                    label="B",
-                                                    values=[3, 1.5],
-                                                ),
-                                                dict(
-                                                    range=[1, 5],
-                                                    tickvals=[
-                                                        1,
-                                                        2,
-                                                        4,
-                                                        5,
-                                                    ],
-                                                    label="C",
-                                                    values=[2, 4],
-                                                    ticktext=[
-                                                        "text 1",
-                                                        "text 2",
-                                                        "text 3",
-                                                        "text 4",
-                                                    ],
-                                                ),
-                                                dict(
-                                                    range=[1, 5],
-                                                    label="D",
-                                                    values=[4, 2],
-                                                ),
-                                            ]
-                                        ),
-                                    )
-                                ),
+                                # figure=None,
                                 style={
                                     "width": "100%",
                                     "height": "100%",
